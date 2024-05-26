ca4732c80749595633e77e448a99c1d8dce623a5
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-05-26 16:53:02 +0500

    created a node server

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 import mysql from 'mysql2'
-// import dotenv from 'dotenv'
-// dotenv.config()
+import dotenv from 'dotenv'
+dotenv.config()
+// console.log(process.env.DB_HOST)
 const pool = mysql.createPool(
     {
-        // host: process.env.MYSQL_HOST,
-        // user: process.env.MYSQL_USER,
-        // password: process.env.MYSQL_PASSWORD,
-        // database: process.env.MYSQL_DATABASE
-
-        host:'127.0.0.1',
-        user: 'root',
-        password: '',
-        database:'booking_db'   }
+        host:process.env.DB_HOST,
+        user:process.env.DB_USERNAME,
+        password:process.env.DB_PASSWORD,
+        database:process.env.DB_DATABASE  
+        // host:'localhost',
+        // user:'root',
+        // password:'',
+        // database:'booking_db'  
+     }
 ).promise();    
-async function getUsers(){
-    const [row] = await pool.query('SELECT * from users')
+async function getUsers(id){
+    const [rows] = await pool.query('SELECT * from users')
     // const row = result[0];
-    console.log(row);
+    // console.log(row);
+    return rows
 }
-const users = await getUsers()
-console.log(users);
+async function getUser(id){
+    const [rows] = await pool.query(`
+    SELECT * 
+    from users 
+    where id= ?`,[id])
+    // return rows
+    return rows[0]
+
+}
+
+const user = await getUser(2)
+console.log(user);
